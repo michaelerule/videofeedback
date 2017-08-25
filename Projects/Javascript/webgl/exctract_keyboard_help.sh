0a2f0e1e3306c00b097ef4ef4f57f0e661ec358a
M Rule <mrule7404@gmail.com>
2017-08-25 16:21:13 +0200

    fix margins on help box in webgl perceptron; help displays at start

--- Projects/Javascript/webgl/exctract_keyboard_help.sh
+++ Projects/Javascript/webgl/exctract_keyboard_help.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-cat ./perceptron_keyboard.js | grep case | grep -Eo '/\*.*?\*/' | sed "s/^\/\*//g" | sed "s/\*\/$//g" | pr -tw134 -3 | python3 -c "import sys; print('var perceptron_help_string=\`\n'+''.join(sys.stdin.readlines())+'\`;')" > "help_string.js"
+# the following command is terrible, I know
+# 
+
+cat ./perceptron_keyboard.js | grep case | grep -Eo '/\*.*?\*/' | grep -Ev 'not bound'  | sed "s/^\/\*//g" | sed "s/\*\/$//g" | pr -tw100 -2 | python3 -c "import sys; print('var perceptron_help_string=\`\n'+''.join(sys.stdin.readlines())+'\`;')" > "help_string.js"
 
 cat ./help_string.js
